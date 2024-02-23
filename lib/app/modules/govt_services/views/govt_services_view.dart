046a682141bc72af19111d4732c6f8013362208d
zihadsikder <zihadsikder.cse@gmail.com>
2024-02-23 22:16:36 +0600

    browser lauch issue fixed

--- lib/app/modules/govt_services/views/govt_services_view.dart
+++ lib/app/modules/govt_services/views/govt_services_view.dart
@@ -9,7 +9,7 @@ import '../../bottom_nav/controllers/bottom_nav_controller.dart';
 import '../controllers/govt_services_controller.dart';
 
 class GovtServicesView extends GetView<GovtServicesController> {
-   GovtServicesView({super.key});
+  GovtServicesView({super.key});
 
   final List<ServiceData> websites = [
     ServiceData(
@@ -45,60 +45,61 @@ class GovtServicesView extends GetView<GovtServicesController> {
   ];
 
   Future<void> _launchUrl(String url) async {
-    if (!await launchUrl(
-        url as Uri)) {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri)) {
       throw Exception('Could not launch $url');
     }
   }
-   @override
-   Widget build(BuildContext context) {
-     // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-     //     statusBarColor: Colors.red.shade800,
-     //     statusBarIconBrightness: Brightness.light));
-     return PopScope(
-       canPop: false,
-       onPopInvoked: (value){
-         Get.find<BottomNavController>().backToHome();
-       },
-       child: SafeArea(
-         child: Scaffold(
-           body: Column(
-             children: [
-               const ProfileSummaryCard(),
-               const SizedBox(height: 16),
-               Expanded(
-                 child: GridView.builder(
-                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                     crossAxisCount: 3,
-                   ),
-                   itemCount: websites.length,
-                   itemBuilder: (context, index) {
-                     return GestureDetector(
-                       onTap: () {
-                         _launchUrl(websites[index].link);
-                       },
-                       child: Card(
-                         elevation: 1,
-                         child: Column(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Image.asset(
-                               websites[index].profilePicAsset,
-                               width: 100,
-                               height: 100,
-                             ),
-                             Text(websites[index].name),
-                           ],
-                         ),
-                       ),
-                     );
-                   },
-                 ),
-               ),
-             ],
-           ),
-         ),
-       ),
-     );
-   }
+
+  @override
+  Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    //     statusBarColor: Colors.red.shade800,
+    //     statusBarIconBrightness: Brightness.light));
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (value) {
+        Get.find<BottomNavController>().backToHome();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              const ProfileSummaryCard(),
+              const SizedBox(height: 16),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: websites.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _launchUrl(websites[index].link);
+                      },
+                      child: Card(
+                        elevation: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              websites[index].profilePicAsset,
+                              width: 100,
+                              height: 100,
+                            ),
+                            Text(websites[index].name),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
